309c58f17bd7712ed3d8daa1c36142188b60a044
BuildxSaas <156997599+BuildxSaasApp@users.noreply.github.com>
2024-02-19 18:34:31 -0300

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -32,9 +32,9 @@ export default function Footer() {
             </div>
 
             <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
-              Nextly is a free landing page & marketing website
+              Nextly is a landing page & marketing website
               template for startups and indie projects. Its built with
-              Next.js & TailwindCSS. And its completely open-source.
+              Next.js & TailwindCSS.
             </div>
 
             <div className="mt-5">
